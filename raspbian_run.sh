d92ae81c6afc84c062d59736d9751b4fc0e7dbcf
Nguyen TienDat <54836300+datnt908@users.noreply.github.com>
2020-05-13 20:08:11 +0700

    Create run projects script & Update README.md

---
+++ raspbian_run.sh
@@ -0,0 +1,4 @@
+cd reactjs
+lxterminal -e yarn start --url=http://0.0.0.0:3000
+cd ../aspnetcore
+dotnet run --urls http://0.0.0.0:5000
---
+++ window_run.bat
@@ -0,0 +1,4 @@
+CD aspnetcore
+START cmd.exe /c dotnet run --urls http://0.0.0.0:5000
+CD ../reactjs
+yarn start --url=http://0.0.0.0:3000
