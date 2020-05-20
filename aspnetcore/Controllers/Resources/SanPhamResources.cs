b124a8a8f95eab58646ae5d87ea6ca3da588e222
duongthanhdat143 <64494386+duongthanhdat143@users.noreply.github.com>
2020-05-20 20:54:08 +0700

    Add API ShowProducts (#7)

---
+++ aspnetcore/Controllers/Resources/SanPhamResources.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcore.Controllers.Resources
+{
+    public class SanPham_ShowProducts_Resource
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public bool RecordStatus { get; set; }
+    }
+}
---
+++ aspnetcore/Controllers/SanPhamController.cs
@@ -0,0 +1,48 @@
+﻿using aspnetcore.Controllers.Resources;
+using aspnetcore.Helper;
+using aspnetcore.Services;
+using aspnetcore.Services.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcore.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SanPhamController:ControllerBase
+    {
+        private readonly ISanPhamService _service;
+        public SanPhamController(ISanPhamService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(List<SanPhamModel>), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(typeof(GeneralResponse), 401)]
+        [ProducesResponseType(typeof(GeneralResponse), 404)]
+        [ProducesResponseType(typeof(GeneralResponse), 500)]
+        public IActionResult ShowProducts([FromBody] SanPham_ShowProducts_Resource sanphamShowProductsResource)
+        {
+            ResultCodes resultCode;
+            List<SanPhamModel> list_sanpham = new List<SanPhamModel>();
+            (resultCode, list_sanpham) = _service.ShowProducts(sanphamShowProductsResource.Code,
+                                                            sanphamShowProductsResource.Name,
+                                                            sanphamShowProductsResource.Category,
+                                                            sanphamShowProductsResource.RecordStatus);
+            Result resultValues; int statusCode;
+            (statusCode, resultValues) = ResultHandler.GetStatusCodeAndResult(resultCode);
+            GeneralResponse response = new GeneralResponse()
+            {
+                Success = (statusCode == 200) ? true : false,
+                Error = resultValues
+            };
+            response.Result = list_sanpham;
+            return StatusCode(statusCode, response);
+        }
+    }
+}
